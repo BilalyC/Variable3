d255e2042ca5cf922f273b061260607155426959
BilalyC <bilaly.camara@outlook.com>
2018-12-17 14:50:35 +0100

    Exercice 3 Complet

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Veuillez saisir une valeur pour a : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir une valeur pour b : ");
+            b = int.Parse(Console.ReadLine());
+
+            a = a + 33;
+            b++;
+
+            Console.WriteLine(a + "/" + b + " = " + (a/b));
+        }
+    }
+}
